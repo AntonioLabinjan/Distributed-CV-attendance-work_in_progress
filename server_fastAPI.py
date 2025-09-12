1b2820a795a878e201e7d11e31c4bf15b2cc4fbf
Antonio Labinjan <alabinjan6@gmail.com>
2025-09-12 11:27:40 +0200

    slight mod in redis setup for docker compose

--- server_fastAPI.py
+++ server_fastAPI.py
@@ -53,9 +53,13 @@ faiss_index = None
 # Log i queue
 detection_log = []
 
-# Redis setup
-redis_client = redis.Redis(host='localhost', port=6379, db=0)  # prilagodi port ako treba
+import os
+import redis
 
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 
 
